e769ef4c31c9cc4a4cd5d8ebc5ca4f866fcdedf6
Mynor Chavarria <mychava96@gmail.com>
2023-12-25 19:00:53 -0600

    Fin de seccion cuatro

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/components/counter/counter.module';
+
+import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.modules';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+
+  ],
+  imports: [
+
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/counter/components/counter/counter.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CounterComponent } from './counter.componet';
+
+
+
+@NgModule({
+  declarations: [
+    CounterComponent
+  ],
+  exports:[
+    CounterComponent
+  ]
+})
+export class CounterModule {
+
+}
